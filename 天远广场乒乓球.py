3e3f9683912825f4d31a8cf40ac46c06ab2397bd
GoJuuZero <1289606085@qq.com>
2024-08-06 00:26:33 +0800

    TableTennis!

---
+++ 天远广场乒乓球.py
@@ -0,0 +1,50 @@
+'''
+乒乓球考虑因素：旋转（上旋、下旋、侧旋）、力量、近台、中台、远台、反应时长、速度、耐力、方向（左中右）、擦网、擦边、发球、受伤
+设定人物：折洛、华柒、懿思、懿范、可奈、杉邃、小区大爷1、小区大爷2、小区大爷3、小区大叔1、小区大叔2、小区大叔3、小区大妈1、小区大妈2、小区青年男1、小区青年男2、小区青年女1、小区青年男2、小区小孩男1、小区小孩男2
+增加十一胜机制，比赛五局三胜
+前六个有特殊技能
+'''
+import random
+print("你可以选择以下6个角色：折洛、华柒、懿思、懿范、可奈、杉邃")
+t0=input("输入你想选的初始角色：")
+
+def zl():
+    pass
+def hq():
+    pass
+def ys():
+    pass
+def yf():
+    pass
+def kn():
+    pass
+def ss():
+    pass
+def dy1():
+    pass
+def dy2():
+    pass
+def dy3():
+    pass
+def ds1():
+    pass
+def ds2():
+    pass
+def ds3():
+    pass
+def dm1():
+    pass
+def dm2():
+    pass
+def qm1():
+    pass
+def qm2():
+    pass
+def qf1():
+    pass
+def qf2():
+    pass
+def xh1():
+    pass
+def xh2():
+    pass
